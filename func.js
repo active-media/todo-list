36e023bd6c3c9e3bcf52dec54949e49180dc7f91
michee <micbakanda@gmail.com>
2023-05-26 16:29:22 +0200

    finished

---
+++ func.js
@@ -0,0 +1,17 @@
+// let list= document.getElementById('myList')
+// function js_func(){
+  
+// let value = document.getElementById("to-do").value;
+// let li = document.createElement("Li");
+// let textNode = document.createTextNode(value);
+// li.appendChild(textNode);
+// list.appendChild(li);
+// }
+
+// list.addEventListener("click", function(ev){
+// if(ev.target.tagName === "LI") {
+//     ev.target.classList.toggle("checked");
+// }
+// }, false)
+
+
